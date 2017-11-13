b493cf7b2332c1f0ed2abb1e87dbddab2c0ff010
andrew54068 <andrew0424718012@yahoo.com.tw>
2017-11-13 12:33:00 +0800

    20171113
    Update

---
+++ DerivedData2/uber driver/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-uber driver.build/DerivedSources/Pods_uber_driver_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_uber_driverVersionString[];
+ extern const double Pods_uber_driverVersionNumber;
+
+ const unsigned char Pods_uber_driverVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_uber_driver  PROJECT:Pods-1" "\n";
+ const double Pods_uber_driverVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData2/uber driver/Build/Intermediates/uber driver.build/Debug-iphoneos/uber driver.build/Script-129F86305B897A7FE5CD91EA.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-uber driver/Pods-uber driver-frameworks.sh"
+
